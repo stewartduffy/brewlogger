e1f4c04a3dabe0968142515757039a47b23a9497
Stewart Duffy <duffy.stewart@gmail.com>
2020-02-25 07:55:09 +1300

    Wraps in cloud function handler

--- index.js
+++ index.js
@@ -1,56 +1,12 @@
-require("dotenv").config();
-const { google } = require("googleapis");
-const axios = require("axios");
+const sheets = require("./sheets");
 
-const init = async () => {
-  const auth = await google.auth.getClient({
-    scopes: ["https://www.googleapis.com/auth/spreadsheets"],
-    keyFilename: "./credentials.json"
-  });
-
-  const sheets = google.sheets({ version: "v4", auth });
-
-  try {
-    const { data } = await sheets.spreadsheets.values.get({
-      spreadsheetId: "1dRau2UdMHiqDWgrnJ6IPyBUkHlowrVXNVAuA-ShtS4k",
-      range: "C2:G2"
-    });
-
-    const rows = data.values;
-    const payload = constructPayload(rows);
-    postReadings(payload);
-  } catch (err) {
-    console.log("The API returned an error: " + err);
-    console.error(err);
-  }
+/**
+ * Responds to any HTTP request.
+ *
+ * @param {!express:Request} req HTTP request context.
+ * @param {!express:Response} res HTTP response context.
+ */
+exports.brewlogger = async (req, res) => {
+  const message = await sheets.main();
+  res.status(200).send(message);
 };
-
-function constructPayload(rows) {
-  const temp = rows[0][2];
-  const gravity = rows[0][1];
-  const beer = rows[0][4];
-
-  return {
-    temp,
-    gravity,
-    beer,
-    name: "Brewlogger",
-    temp_unit: "C",
-    gravity_unit: "G",
-    comment: "Hello World"
-  };
-}
-
-async function postReadings(payload) {
-  const BREWFATHER_STREAM_ID = process.env.BREWFATHER_STREAM_ID;
-  const BREWFATHER_ENDPOINT = `http://log.brewfather.net/stream?id=${BREWFATHER_STREAM_ID}`;
-
-  try {
-    const { data } = await axios.post(BREWFATHER_ENDPOINT, payload);
-    console.log(data);
-  } catch (error) {
-    console.error(error);
-  }
-}
-
-init();
---
+++ sheets.js
@@ -0,0 +1,61 @@
+require("dotenv").config();
+const { google } = require("googleapis");
+const axios = require("axios");
+
+function constructPayload(rows) {
+  const temp = rows[0][2];
+  const gravity = rows[0][1];
+  const beer = rows[0][4];
+
+  return {
+    temp,
+    gravity,
+    beer,
+    name: "Brewlogger",
+    temp_unit: "C",
+    gravity_unit: "G",
+    comment: "Hello World"
+  };
+}
+
+async function postReadings(payload) {
+  const BREWFATHER_STREAM_ID = process.env.BREWFATHER_STREAM_ID;
+  const BREWFATHER_ENDPOINT = `http://log.brewfather.net/stream?id=${BREWFATHER_STREAM_ID}`;
+
+  try {
+    const { data } = await axios.post(BREWFATHER_ENDPOINT, payload);
+    console.log(data);
+    return data;
+  } catch (error) {
+    console.error(error);
+    return error;
+  }
+}
+
+const main = async () => {
+  const auth = await google.auth.getClient({
+    scopes: ["https://www.googleapis.com/auth/spreadsheets"],
+    keyFilename: "./credentials.json"
+  });
+
+  const sheets = google.sheets({ version: "v4", auth });
+
+  try {
+    const { data } = await sheets.spreadsheets.values.get({
+      spreadsheetId: "1dRau2UdMHiqDWgrnJ6IPyBUkHlowrVXNVAuA-ShtS4k",
+      range: "C2:G2"
+    });
+
+    const rows = data.values;
+    const payload = constructPayload(rows);
+    const response = await postReadings(payload);
+    console.log("response ", response);
+    return response;
+  } catch (err) {
+    console.log("The API returned an error: " + err);
+    console.error(err);
+    throw new Error(err);
+  }
+};
+
+exports.main = main;
